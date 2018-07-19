642ac8b084ea943545548b050d05d1df57a83aaf
sandeepa <sandeepajayasanka@gmail.com.com>
2018-07-19 22:48:02 +0530

    Just a ESLint Warnings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,17 @@
-module.exports = (grunt) => {
-  grunt.initConfig({
-    execute: {
-      target: {
-        src: ['app.js']
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['app.js'],
-        tasks: ['execute'],
-      },
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-execute');
-};
+export default function (grunt) {
+    grunt.initConfig({
+        execute: {
+            target: {
+                src: ['app.js']
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['app.js'],
+                tasks: ['execute']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-execute');
+}
